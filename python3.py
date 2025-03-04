2a98f688d472903c72079f91ee9b2b852d4a2c6d
Atulya Kushwaha <atulya1234567890@gmail.com>
2025-03-05 02:22:48 +0900

    improvementin code

--- python3.py
+++ python3.py
@@ -1,6 +1,6 @@
 operation =input("Enter an operation + - * / %")
-num1 = int(input("Enter first number " ))
-num2 = int(input("Enter secend number "))
+num1 = float(input("Enter first number " ))
+num2 = float(input("Enter secend number "))
 if operation == "+" :
      result=num1+num2
 elif operation =="-":
@@ -11,4 +11,6 @@ elif operation == "/":
      result = num1 / num2
 elif operation == "%":
      result= num1 % num2
-print("The number is", result)
+else:
+     print("The number invalid")
+print("The number is",round(result,3))
