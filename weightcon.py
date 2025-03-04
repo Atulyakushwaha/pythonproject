42fba6c0c0df661f5cdf284c517160f56c6b3bfd
Atulya Kushwaha <atulya1234567890@gmail.com>
2025-03-05 02:24:37 +0900

    new code of project

---
+++ weightcon.py
@@ -0,0 +1,13 @@
+weight= float(input("Enter your weight"))
+unit = input("kilograms or pounds? (K or L)")
+
+if unit.upper()=="K":
+    weight=weight*2.205
+    uni="lbs"
+    print(f"your weight is : {round(weight,1)} {uni}")
+elif unit.upper()=="L":
+    weight=weight*2.20
+    uni="kgs"
+    print(f"your weight is : {round(weight,1)} {uni}")
+else:
+    print(f"{unit} was not vaild ")
